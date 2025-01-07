fbe0be777c245842fd8877243ac155c285d2970c
bjvanbemmel <beauvanbemmel@live.nl>
2025-01-07 15:19:15 +0000

    Read messages from queue

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -11,26 +12,41 @@ import (
 )
 
 func main() {
+  time.Sleep(time.Second * 5)
+
   r := chi.NewRouter()
   r.Use(middleware.Logger)
   r.Use(middleware.StripSlashes)
 
-  conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@rabbitmq:5672/", os.Getenv("MQ_USERNAME"), os.Getenv("MQ_PASSWORD")))
+  conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@rabbitmq", os.Getenv("MQ_USERNAME"), os.Getenv("MQ_PASSWORD")))
   if err != nil {
     panic(err)
   }
 
   channel, err := conn.Channel()
+  if err != nil {
+    panic(err)
+  }
   defer channel.Close()
 
-  queue, err := channel.QueueDeclare(
+  messages, err := channel.Consume(
     "queuepods",
+    "api",
     false,
     false,
     false,
     false,
     nil,
   )
+  if err != nil {
+    panic(err)
+  }
+
+  go func() {
+    for msg := range messages {
+      fmt.Println(string(msg.Body))
+    }
+  }()
 
   r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Pong!")
